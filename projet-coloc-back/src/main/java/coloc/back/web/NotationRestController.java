4f4a6e634ce25b36edc8bef032ea61f10824e483
Emma Faurie <emma.iris.faurie@gmail.com>
2021-11-24 15:38:13 +0100

    RestController (Notation, Photo, Proprietaire)

---
+++ projet-coloc-back/src/main/java/coloc/back/web/NotationRestController.java
@@ -0,0 +1,85 @@
+package coloc.back.web;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+
+
+import coloc.back.model.Notation;
+import coloc.back.repository.INotationRepository;
+
+
+@RestController
+@RequestMapping("/notation")
+@CrossOrigin("*")
+public class NotationRestController {
+
+	@Autowired
+	private INotationRepository notationRepo;
+
+	@GetMapping("")
+//	@JsonView(Views.ViewNotation.class)
+	public List<Notation> findAll() {
+		List<Notation> notations = notationRepo.findAll();
+
+		return notations;
+	}
+
+	@GetMapping("{id}")
+//	@JsonView(Views.ViewNotation.class)
+	public Notation find(@PathVariable Long id) {
+		Optional<Notation> optNotation = notationRepo.findById(id);
+
+		if (optNotation.isPresent()) {
+			return optNotation.get();
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Notation non trouvée");
+		}
+	}
+	
+
+	@PostMapping("")
+//	@JsonView(Views.ViewNotation.class)
+	public Notation create(@RequestBody Notation notation) {
+		notation = notationRepo.save(notation);
+
+		return notation;
+	}
+
+	@PutMapping("/{id}")
+	//@JsonView(Views.ViewNotation.class)
+	public Notation update(@PathVariable Long id, @RequestBody Notation notation) {
+		if (!notationRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Notation non trouvée");
+		}
+
+		notation = notationRepo.save(notation);
+
+		return notation;
+	}
+
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		if (!notationRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Notation non trouvée");
+		}
+		
+		notationRepo.deleteById(id);
+	}
+
+}
