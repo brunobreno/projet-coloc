73047d165a366dede78ea1e1f0ec00c8655941f1
Guillaume <nguyen.guillaume@outlook.com>
2021-12-02 16:57:03 +0100

    Page profilLocataire

--- projet-coloc-front/src/app/app-routing.module.ts
+++ projet-coloc-front/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfilLocataireComponent } from './profil-locataire/profil-locataire.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "profilLocataire", component: ProfilLocataireComponent},
+  { path: "", redirectTo: "profilLocataire", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projet-coloc-front/src/app/app.module.ts
+++ projet-coloc-front/src/app/app.module.ts
@@ -6,10 +6,15 @@ import { AppConfigService } from './app-config.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfilLocataireComponent } from './profil-locataire/profil-locataire.component';
+import { LocataireComponent } from './locataire/locataire.component';
+import { LocataireHttpService } from './locataire/locataire-http.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfilLocataireComponent,
+    LocataireComponent
   ],
   imports: [
     BrowserModule,
@@ -17,7 +22,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpClientModule
   ],
-  providers: [AppConfigService],
+  providers: [AppConfigService,LocataireHttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
