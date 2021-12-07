2de1b2e87393a26ec98eab795a5bbd643ed9531c
Bruno BASTIER <bastierbruno@gmail.com>
2021-12-07 11:36:13 +0100

    redirections sur home OK

--- projet-coloc-front/src/app/home-page/home-page.component.ts
+++ projet-coloc-front/src/app/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AppConfigService } from '../app-config.service';
 import { Logement } from '../model';
@@ -13,14 +14,33 @@ import { HomePageService } from './home-page.service';
 export class HomePageComponent implements OnInit {
 
   logementRecents = new Array<Logement>();
+  saisieVille: string;
 
-  constructor(private homeService: HomePageService, private appConfig: AppConfigService) { 
+  constructor(private homeService: HomePageService, private appConfig: AppConfigService, private router: Router) { 
      homeService.loadMostRecentLogements().subscribe(resp => {
        this.logementRecents = resp;
+       this.logementRecents.forEach(log => {
+         this.homeService.loadPhotoByLogement(log.id).subscribe(resp => {
+           log.photos = resp;
+         }, err => console.log)
+       });
      }, err => console.log(err))
   }
 
   ngOnInit(): void {
+  }
+
+  redirect(id:number){
+    this.router.navigate(["./description-logement/", id]);
+  }
+
+  redirectVille(ville: string){
+    this.router.navigate(["./recherche-logement/", ville]);
+  }
+
+  rechercheVille(){
+    this.redirectVille(this.saisieVille);
+    this.saisieVille = null;
   }
 
 }
